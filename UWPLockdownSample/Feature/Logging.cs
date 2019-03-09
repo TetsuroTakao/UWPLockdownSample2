53a9897ea2957fc26bc8bbd1d2551b14d89886f4
髙尾 <tetsuro.takao@processtune.com>
2019-03-10 02:31:39 +0900

    Impliment logging

---
+++ UWPLockdownSample/Feature/Logging.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWPLockdownSample.Model;
+using Windows.Globalization.DateTimeFormatting;
+using Windows.Storage;
+
+namespace UWPLockdownSample.Feature
+{
+    public class Logging
+    {
+        public StorageFile LogFile { get; set; }
+        public Logging()
+        {
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+
+            DateTimeFormatter formatter = new DateTimeFormatter("longtime");
+
+            new Task(async () => { LogFile = await localFolder.CreateFileAsync("dataFile.txt", CreationCollisionOption.ReplaceExisting); }).RunSynchronously();
+        }
+        public void AppendWrite(List<LogModel> logs,FileInfo logfile)
+        {
+            List<LogModel> old = new List<LogModel>();
+            string jsonstring = string.Empty;
+            new Task(async () => 
+            {
+                jsonstring = await FileIO.ReadTextAsync(LogFile);
+            }).RunSynchronously();
+            if (!string.IsNullOrEmpty(jsonstring))
+            {
+                logs.AddRange(JsonConvert.DeserializeObject<List<LogModel>>(jsonstring));
+            }
+            new Task(async () => 
+            {
+                await FileIO.WriteTextAsync(LogFile, JsonConvert.SerializeObject(logs));
+            }).RunSynchronously();
+        }
+    }
+}
---
+++ UWPLockdownSample/Model/LogModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UWPLockdownSample.Model
+{
+    public class LogModel
+    {
+        public string Message { get; set; }
+        public DateTime OccurredTime { get; set; }
+        public string OperatorName { get; set; }
+        public LogType LogType { get; set; }
+    }
+
+    public enum LogType
+    {
+        Default = 0,
+        Information = 1,
+        Error = 2,
+        Operation = 4
+    }
+}
